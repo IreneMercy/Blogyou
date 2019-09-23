7fcee08d43e9a01a356dcf240ab4ad7e8d9c035b
IreneMercy <irenemercy700@gmail.com>
2019-09-23 09:15:21 +0300

    add app configurations

---
+++ config.py
@@ -0,0 +1,10 @@
+class Config:
+    Debug = True
+    SECRET_KEY= '4444'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://joozao:12345@localhost/blogyu'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME='ijanemercy@gmail.com'
+    MAIL_PASSWORD='@janeMercy700'
