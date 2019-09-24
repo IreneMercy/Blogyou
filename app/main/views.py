2c4aac852af96a5307086b311d158037c96a8e97
IreneMercy <irenemercy700@gmail.com>
2019-09-24 17:29:57 +0300

    add update profile and profile-pic function

--- app/main/views.py
+++ app/main/views.py
@@ -93,3 +93,29 @@ def profile(uname):
     if user is None:
         abort(404)
     return render_template("profile/profile.html", user=user)
+
+
+
+@main.route('/user/<uname>/update', methods=['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+    if request.method == "POST":
+        user.bio = request.form.get('bio')
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('main.profile',uname=user.username))
+    return render_template('profile/profile.html', user=user)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'images/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
