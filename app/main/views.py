02a166c597b118084506ad26a0e9dea2655e96a0
IreneMercy <irenemercy700@gmail.com>
2019-09-24 18:30:57 +0300

    add blog to profile

--- app/main/views.py
+++ app/main/views.py
@@ -93,7 +93,7 @@ def profile(uname):
     if user is None:
         abort(404)
     blogs = Blog.query.filter_by(user_id= current_user.id).all()
-    return render_template("profile/profile.html", user=user)
+    return render_template("profile/profile.html", user=user, blogs=blogs)
 
 
 
