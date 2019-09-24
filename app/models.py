fd8b68d7864a1fd1af50161bbadbc1f4904ec0e8
IreneMercy <irenemercy700@gmail.com>
2019-09-24 08:15:26 +0300

    add subscribers table

--- app/models.py
+++ app/models.py
@@ -12,7 +12,9 @@ class User(UserMixin,db.Model):
     password = db.Column(db.String(255), nullable=False)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String(255))
-    posts = db.relationship('Post', backref='blogyou',lazy=True)
+    blogs = db.relationship('Blog', backref='blogyou',lazy=True)
+    comment = db.relationship('Comments', backref='commenting',lazy=True)
+
 
     def save(self):
         db.session.add(self)
@@ -33,18 +35,18 @@ class User(UserMixin,db.Model):
         return "User: %s"%str(self.username)
 
 
+@login_manager.user_loader
+def user_loader(user_id):
+    return User.query.get(user_id)
 
 
 class Blog(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    category = db.Column(db.String(50))
     title = db.Column(db.String(100),nullable=False)
     date_posted = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
-    content = db.Column(db.String(255),nullable=False)
+    blog = db.Column(db.Text,nullable=False)
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable=False)
-
-
-
+    comment_it = db.relationship('Comments', backref='user_comment',lazy=True)
 
     def save(self):
         db.session.add(self)
@@ -56,9 +58,46 @@ class Blog(db.Model):
 
 
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
+        return f"Blog('{self.title}', '{self.date_posted}')"
 
 
-    @login_manager.user_loader
-    def user_loader(user_id):
-        return User.query.get(user_id)
+
+
+class Comments(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    comment = db.Column(db.String(255),nullable=False)
+    date_posted = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    blog_id = db.Column(db.Integer,db.ForeignKey('blog.id'))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+    def __repr__(self):
+        return f"Comments('{self.comment}', '{self.date_posted}')"
+
+class Subscribe(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(100))
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    def __repr__(self):
+        return f"Comments('{self.email}')"
+
+
+class Quotes:
+    def __init__(self,author,quote,permalink):
+        self.author = author
+        self.quote = quote
+        self.permalink = permalink
